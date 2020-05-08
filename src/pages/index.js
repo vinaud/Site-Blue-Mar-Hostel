933aecc7e9ca5ce0c2d881adc327def701ca0557
João Vinaud <leyenasd@gmail.com>
2020-05-08 09:41:27 -0300

    Aplicação do helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Helmet} from 'react-helmet';
 
 const Index = () => {
     return(
         <div>
+            <Helmet>
+                 <title>Blue Mar Hostel</title>
+            </Helmet>
             <h1> Hello Warudo </h1>
         </div>
     )
