7506c0edbc02576313c747dcfa242828769adce3
João Vinaud <leyenasd@gmail.com>
2020-05-12 16:30:55 -0300

    Botão clicável

--- src/components/BtnContato.js
+++ src/components/BtnContato.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import wa from '../assets/btn-whatsapp.png';
+//import {Link} from 'react-router-dom'
 
 
 const BtnContato = () => {
     return (
-        <div className='bg-white shadow-2xl rounded px-5 py-2 flex flex-col items-center mt-6 sm:mt-0 sm:flex-row'>
+        <div>
+        <button
+        className='bg-white shadow-2xl rounded px-5 py-2 flex flex-col items-center mt-6 sm:mt-0 sm:flex-row hover:bg-blue-700 '
+        type="button"
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.href='https://api.whatsapp.com/send?phone=1111111111';
+          }}
+    >
                <img className="object-scale-down h-20 w-20"  src={wa} alt='Whatsapp'/>
                <div className='ml-2'>
                    <h3 className='font-bold text-2xl'>Reserve conosco</h3>
                    <p className='text-xs'>Faça seu orçamento sem compromisso</p>
                    <p className='font-bold'>+55 (84) 981276722</p>
                </div>
-            </div>
+               </button>
+        
+       </div>
     )
 
 }
