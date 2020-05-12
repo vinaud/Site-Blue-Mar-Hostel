784f6eb6d9e840e5e52d4f0f63061bbecd2191bd
João Vinaud <leyenasd@gmail.com>
2020-05-12 15:08:16 -0300

    componente Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BtnContato from '../components/BtnContato';
+import logo from '../components/logo';
+
+const Header = () => {
+    return(
+        <div>
+        
+        </div>
+    )
+}
+
+export default Header;
