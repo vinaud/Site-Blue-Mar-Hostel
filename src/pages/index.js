7cda866e7fd3ae6e1360a899f4d1f56e2d7aa4f1
João Vinaud <leyenasd@gmail.com>
2020-05-14 16:21:07 -0300

    Melhoria visual no hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,12 @@ import heroImg from '../assets/hero.jpg';
 const Hero = () => {
     
     return(
-        <div className='flex flex-col items-center sm: bg-cover bg-center text-center justify-between bg-fixed ' style={{ backgroundImage: `url(${heroImg})`  }}  >
-              <div className='p-8'>
-                  <h2 className='font-bold text-6xl text-white text-center '>Sua casa fora de casa</h2>
-                  <p className='text-2xl text-white '>Localizado bem pertinho da praia de Ponta Negra</p>
-                  <p className='text-gray-400'>Faça sua reserva conosco </p>
-                
+        <div className='p-32 flex flex-col items-center  sm: bg-cover bg-center  text-center justify-between bg-fixed ' style={{ backgroundImage: `url(${heroImg})` }}  >
+              <div className='  rounded-lg p-8 bg-gray-300 bg-opacity-50'>
+                  <h2 className='font-bold text-6xl text-black text-center '>Sua casa fora de casa</h2>
+                  <p className='text-2xl text-black '>Localizado bem pertinho da praia de Ponta Negra</p>
+                  <p className='text-2xl text-black'>Faça sua reserva conosco </p>
+                  
               </div>
               
           </div>
