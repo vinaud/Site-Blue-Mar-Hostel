204d2c4d8067f648cfa0816ea6afb8a254a6ac8b
João Vinaud <leyenasd@gmail.com>
2020-05-18 17:18:56 -0300

    botões de acesso a redes sociais

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import BtnContato from '../components/BtnContato';
+import facebook from '../assets/facebook.png';
+import insta from '../assets/instagram.png';
 
 
 const Footer = () => {
@@ -10,7 +12,14 @@ const Footer = () => {
               <h5>Blue Mar Hostel</h5>
               <p>Rua Luiz Estevam, 94, Ponta Negra, Natal/RN</p>
             </div>
-
+            <div className = 'sm:flex justify-between '>
+              <a href="https://www.facebook.com/bluemarhostelnatal" target="_blank" rel="noopener noreferrer">
+                  <img className="object-scale-down h-20 w-20 p-4"  src={facebook} alt='Facebook'/>
+              </a>
+              <a href="https://www.instagram.com/bluemarhostelnatal/" target="_blank" rel="noopener noreferrer">
+                  <img className="object-scale-down h-20 w-20 p-4"  src={insta} alt='Instagram'/>
+              </a>
+            </div>
             <BtnContato/>
           
           </footer>
