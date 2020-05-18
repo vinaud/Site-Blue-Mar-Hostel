f3fb6b16ba70d80c9d57800eced0e74e370b1ead
João Vinaud <leyenasd@gmail.com>
2020-05-18 14:48:15 -0300

    Placeholder da listagem de quartos

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import Room from '../entities/room.js';
 
 const Selos = () => {
     return(
-        <div className='pb-8 bg-gray-400 flex flex-col sm:flex-row justify-around'>
+        <div className='pb-8 bg-gray-200 flex flex-col sm:flex-row justify-around'>
     <div className='flex flex-col justify-around'>
         <img className="my-4 object-scale-down h-64 w-64" src={cafe} alt="Café da manhã"/>
         <h1 className="text-xl font-bold  text-black text-center ">Café da manhã completo</h1>
@@ -28,7 +28,8 @@ const Selos = () => {
 }
 
 const Hero = () => {
-    let quarto1 = new Room("","","");
+  
+    
     return(
         <div className='p-32 flex flex-col items-center  sm: bg-cover bg-center  text-center justify-between bg-fixed ' style={{ backgroundImage: `url(${heroImg})` }}  >
               <div className='  rounded-lg p-8 bg-gray-300 bg-opacity-50'>
@@ -44,6 +45,13 @@ const Hero = () => {
 }
 
 const Index = () => {
+    const quartos = [new Room("Plâncton","",""),
+    new Room("Amazonas","",""),
+    new Room("Cisne Branco","",""),
+    new Room("Endurance","",""),
+    new Room("Bounty","",""),
+    new Room("Atrevida","","")];
+
     return(
         <div>
             <Helmet>
@@ -52,6 +60,24 @@ const Index = () => {
             <Header/>
             <Hero/>
             <Selos/>
+            <div>
+              <h2 className='px-8 py-2 text-4xl font-bold'>Nossos quartos:</h2>
+              <div className='flex flex-wrap justify-around'>
+              {quartos.map((quarto) => {
+                     return(
+                       <div className='max-w-sm'>
+                        <div className=' m-6 rounded shadow-lg'>
+                            <img  src={require('../assets/cafe.jpg')} alt='Quarto'/>
+                            <div className='px-6 py-4'>
+                                <p className ='font-bold text-xl mb-2'>{quarto.name}</p>
+                                <p>Descrição</p>
+                            </div>
+                        </div>
+                      </div>
+                     )
+              })}
+              </div>
+          </div>
             <Footer/>
         </div>
     )
